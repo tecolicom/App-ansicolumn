831466b571e6a28ad3dfce35e45762cad2ab1b9d
Kazumasa Utashiro <kaz@utashiro.com>
2020-09-30 12:44:12 +0900

    minil new App-ansicolumn

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::ansicolumn - It's new $module
+
+# SYNOPSIS
+
+    use App::ansicolumn;
+
+# DESCRIPTION
+
+App::ansicolumn is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/App/ansicolumn.pm
@@ -0,0 +1,39 @@
+package App::ansicolumn;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::ansicolumn - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::ansicolumn;
+
+=head1 DESCRIPTION
+
+App::ansicolumn is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
